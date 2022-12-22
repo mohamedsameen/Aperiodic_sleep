e5e44a490e79a7d3806124d0caecb10db0e58afb
Ameen Mohamed <mohamed.ameen@plus.ac.at>
2022-12-22 01:04:28 +0100

    added eelctrode info for changing electodes to use for analysis

--- Scripts/PSD_Group_level.py
+++ Scripts/PSD_Group_level.py
@@ -25,22 +25,24 @@ subj_files = sorted(subj_files)
 # Settings for the analyses
 fs = 250
 SETTINGS_B = { 'method' : 'welch', 'average' : 'mean', 'fmin' :1 }
-path_results = Path('/home/b1044271/Columbia/Results/PSD')
+path_results = Path('/home/b1044271/Columbia/Results/PSDs')
 Time_segments = [2, 5, 10, 15] #in seconds
 f_max = [30, 45 , 55, 70, 100]
-
+electrode = 'E21'
 # Helper function for paths
 def check_path(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
+check_path(path_results / electrode)
+
 i = 0
 for x in subj_files:
 
-  check_path(path_results / x[0:4]) # create subject folder
+  check_path(path_results / electrode / x[0:4]) # create subject folder
   EEG = mne.io.read_raw_eeglab(os.path.join(Data_path, x)) # read raw .set file
   print('loaded')
-  EEG1 = EEG.pick('E257', exclude=[]) # select Cz for further analysis
+  EEG1 = EEG.pick(electrode, exclude=[]) # select Cz for further analysis
   events = mne.read_events(Path(path_stage, stage_files[i])) #read staging markers
   epochs = mne.Epochs(EEG, events=events, tmin=-30, tmax=0)
   i = i+1
@@ -67,16 +69,20 @@ for x in subj_files:
           AW = AWs.compute_psd(**SETTINGS_B, fmax=f, n_fft=fs*t)
           RM = REs.compute_psd(**SETTINGS_B, fmax=f, n_fft=fs*t)
 
-          check_path(path_results / x[0:4] / 'N1')
-          check_path(path_results / x[0:4] / 'N2')
-          check_path(path_results / x[0:4] / 'N3')
-          check_path(path_results / x[0:4] / 'AW')
-          check_path(path_results / x[0:4] / 'RM')
+          check_path(path_results /electrode/ x[0:4] / 'N1')
+          check_path(path_results /electrode/ x[0:4] / 'N2')
+          check_path(path_results /electrode/ x[0:4] / 'N3')
+          check_path(path_results /electrode/ x[0:4] / 'AW')
+          check_path(path_results /electrode/ x[0:4] / 'RM')
 
-          names = ['T' ,str(t), '_F' , str(f), '.txt']
+          names = ['T' ,str(t), '_F' , str(f), '.npy']
+          Freq_res = ['F' , str(f), '_res.npy']
 
-          np.savetxt(Path(path_results / x[0:4] / 'N1'/ ''.join(names)),np.squeeze(N1._data))
-          np.savetxt(Path(path_results / x[0:4] / 'N2'/ ''.join(names)),np.squeeze(N2._data))
-          np.savetxt(Path(path_results / x[0:4] / 'N3'/ ''.join(names)),np.squeeze(N3._data))
-          np.savetxt(Path(path_results / x[0:4] / 'AW'/ ''.join(names)),np.squeeze(AW._data))
-          np.savetxt(Path(path_results / x[0:4] / 'RM'/ ''.join(names)),np.squeeze(RM._data))
+
+          np.save(Path(path_results /electrode/ x[0:4] / 'N1'/ ''.join(names)),np.squeeze(N1._data))
+          np.save(Path(path_results /electrode/ x[0:4] / 'N2'/ ''.join(names)),np.squeeze(N2._data))
+          np.save(Path(path_results /electrode/ x[0:4] / 'N3'/ ''.join(names)),np.squeeze(N3._data))
+          np.save(Path(path_results /electrode/ x[0:4] / 'AW'/ ''.join(names)),np.squeeze(AW._data))
+          np.save(Path(path_results /electrode/ x[0:4] / 'RM'/ ''.join(names)),np.squeeze(RM._data))
+
+          np.save(Path(path_results / ''.join(Freq_res)),RM._freqs)
