2e20eb997dbb25276fab4698c001a1ff4f909470
Tom Donoghue <tdonoghue.research@gmail.com>
2023-01-10 16:48:07 -0500

    udpate naming schemes

--- helpers/plots.py
+++ helpers/plots.py
@@ -2,7 +2,7 @@
 
 from matplotlib import cm
 
-from mne.viz import plot_topomap
+import mne
 
 ###################################################################################################
 ###################################################################################################
@@ -10,6 +10,6 @@ from mne.viz import plot_topomap
 def plot_topo(data, raw, vmin, vmax, label=None, ax=None):
     """Plots the topography of a spectral parameter."""
 
-    plot_topomap(data, raw.info, vmin=vmin, vmax=vmax,
-                 cmap=cm.viridis, contours=0, axes=ax);
+    mne.viz.plot_topomap(data, raw.info, vmin=vmin, vmax=vmax,
+                         cmap=cm.viridis, contours=0, axes=ax);
     ax.set_title(label)
