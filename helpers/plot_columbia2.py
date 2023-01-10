073a3d3f201e644f1e6b09d7068b27a0ace55675
Tom Donoghue <tdonoghue.research@gmail.com>
2023-01-10 16:37:15 -0500

    fix topo func

--- helpers/plot_columbia2.py
+++ helpers/plot_columbia2.py
@@ -1,7 +1,15 @@
-def plot_hd_topo(data, raw, zmin, zmax, ax):
+"""Helper plot functions for the sleep project"""
+
+from matplotlib import cm
+
+from mne.viz import plot_topomap
+
+###################################################################################################
+###################################################################################################
+
+def plot_topo(data, raw, vmin, vmax, label=None, ax=None):
     """Plots the topography of a spectral parameter."""
 
-    
-    mne.viz.plot_topomap(data, raw.info, vmin=zmin, vmax=zmax,
-                         cmap=cm.viridis, contours=0, axes=ax);
+    plot_topomap(data, raw.info, vmin=vmin, vmax=vmax,
+                 cmap=cm.viridis, contours=0, axes=ax);
     ax.set_title(label)
