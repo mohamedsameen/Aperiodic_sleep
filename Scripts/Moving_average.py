5da3ba787b10c84505165357a4b0000587dc039d
Ameen Mohamed <mohamed.ameen@plus.ac.at>
2023-01-11 07:07:13 +0100

    Adjusted moving average script, added options (whole night, single subject,...etc)

--- Scripts/Moving_average.py
+++ Scripts/Moving_average.py
@@ -9,6 +9,17 @@ from fooof import FOOOFGroup
 # Import a utility to download and load example data
 from fooof.utils.download import load_fooof_data
 from pathlib import Path
+# importing the random module
+import random
+
+
+# SET THE IMPORTANT parameters
+
+Time_segment = 5 #in seconds
+Overlap = 4
+electrode = 'E257'
+single_subject = 1
+whole_night = 1
 
 # get the staging list
 path_stage = Path('/home/b1044271/EEGsleep/SleepStaging/mat/mne2/')
@@ -24,13 +35,11 @@ subj_files = sorted(subj_files)
 
 # Settings for the analyses
 fs = 250
-SETTINGS_B = { 'method' : 'welch', 'average' : 'mean', 'fmin' :1 }
-path_results = Path('/home/b1044271/Columbia/Results/PSDs')
-path_frqs = Path('/home/b1044271/Columbia/Results/PSDs/Freqs')
+SETTINGS_B = { 'method' : 'welch', 'average' : 'mean', 'fmin' :1 , 'fmin' : 45 , 'n_fft': fs*Time_segment}
+path_results = Path('/home/b1044271/Columbia/Results/Time-resolved')
 
-Time_segments = [2, 5, 10, 15] #in seconds
-f_max = [30, 45 , 55, 70, 100]
-electrode = 'E137'
+
+
 # Helper function for paths
 def check_path(path):
     if not os.path.exists(path):
@@ -38,53 +47,52 @@ def check_path(path):
 
 check_path(path_results / electrode)
 
-i = 0
-for x in subj_files:
 
-  check_path(path_results / electrode / x[0:4]) # create subject folder
-  EEG = mne.io.read_raw_eeglab(os.path.join(Data_path, x)) # read raw .set file
-  print('loaded')
-  EEG1 = EEG.pick(electrode, exclude=[]) # select Cz for further analysis
-  events = mne.read_events(Path(path_stage, stage_files[i])) #read staging markers
-  epochs = mne.Epochs(EEG, events=events, tmin=-30, tmax=0)
-  i = i+1
-# Frequency transformation welch's method from 1 to 70Hz different time stamps
-    #Now we select epochs and average over all epochs
-  N1s = epochs['1']
-  N2s = epochs['2']
-  N3s = epochs['3']
-  REs = epochs['5']
-  AWs = epochs['0']
+if single_subject:
+    EEG     = mne.io.read_raw_eeglab(os.path.join(Data_path, subj_files[random.randint(0,16)])) # read raw .set file
+    EEG_seg = mne.make_fixed_length_epochs(EEG, duration = Time_segments, reject_by_annotation = 'True', overlap = Overlap)
+    X       = EEG_seg.compute_psd(**SETTINGS_B)
 
-   # Settings for PSD calculation using welch's
-  N1={}
-  N2={}
-  N3={}
-  AW={}
-  RM={}
-
-  for t in Time_segments:
-      for f in f_max:
-          N1 = N1s.compute_psd(**SETTINGS_B, fmax=f, n_fft=fs*t) #N1
-          N2 = N2s.compute_psd(**SETTINGS_B, fmax=f, n_fft=fs*t) #N2
-          N3 = N3s.compute_psd(**SETTINGS_B, fmax=f, n_fft=fs*t)
-          AW = AWs.compute_psd(**SETTINGS_B, fmax=f, n_fft=fs*t)
-          RM = REs.compute_psd(**SETTINGS_B, fmax=f, n_fft=fs*t)
-
-          check_path(path_results /electrode/ x[0:4] / 'N1')
-          check_path(path_results /electrode/ x[0:4] / 'N2')
-          check_path(path_results /electrode/ x[0:4] / 'N3')
-          check_path(path_results /electrode/ x[0:4] / 'AW')
-          check_path(path_results /electrode/ x[0:4] / 'RM')
-
-          names = ['T' ,str(t), '_F' , str(f), '.npy']
-          Freq_res = ['T' ,str(t),'F' , str(f), '_freqres.npy']
+    fm1.fit(X._freqs, np.squeeze(X._data), [X._freqs[0] , X._freqs[-1]])
+    fm2.fit(X._freqs, np.squeeze(X._data), [X._freqs[0] , X._freqs[-1]])
 
 
-          np.save(Path(path_results /electrode/ x[0:4] / 'N1'/ ''.join(names)),np.squeeze(N1._data))
-          np.save(Path(path_results /electrode/ x[0:4] / 'N2'/ ''.join(names)),np.squeeze(N2._data))
-          np.save(Path(path_results /electrode/ x[0:4] / 'N3'/ ''.join(names)),np.squeeze(N3._data))
-          np.save(Path(path_results /electrode/ x[0:4] / 'AW'/ ''.join(names)),np.squeeze(AW._data))
-          np.save(Path(path_results /electrode/ x[0:4] / 'RM'/ ''.join(names)),np.squeeze(RM._data))
 
-          np.save(Path(path_frqs / ''.join(Freq_res)),RM._freqs)
+else:
+    i = 0
+    for x in subj_files:
+
+        if whole_night:
+            EEG     = mne.io.read_raw_eeglab(os.path.join(Data_path, subj_files[random.randint(0,16)])) # read raw .set file
+            EEG_seg = mne.make_fixed_length_epochs(EEG, duration = Time_segments, reject_by_annotation = 'True', overlap = Overlap)
+            X       = EEG_seg.compute_psd(**SETTINGS_B)
+
+            fm1.fit(X._freqs, np.squeeze(X._data), [X._freqs[0] , X._freqs[-1]])
+            fm2.fit(X._freqs, np.squeeze(X._data), [X._freqs[0] , X._freqs[-1]])
+
+
+        else:
+
+            events = mne.read_events(Path(path_stage, stage_files[i])) #read staging markers
+            epochs = mne.Epochs(EEG, events=events, tmin=-30, tmax=0)
+            i = i+1
+            Frequency transformation welch's method from 1 to 70Hz different time stamps
+                #Now we select epochs and average over all epochs
+            N1s = epochs['1']
+            N2s = epochs['2']
+            N3s = epochs['3']
+            REs = epochs['5']
+            AWs = epochs['0']
+
+            # Settings for PSD calculation using welch's
+            N1={}
+            N2={}
+            N3={}
+            AW={}
+            RM={}
+
+            N1 = N1s.compute_psd(**SETTINGS_B) #N1
+            N2 = N2s.compute_psd(**SETTINGS_B) #N2
+            N3 = N3s.compute_psd(**SETTINGS_B)
+            AW = AWs.compute_psd(**SETTINGS_B)
+            RM = REs.compute_psd(**SETTINGS_B)
